5bbdcea8f4df779419ee1068c55e83a632d22ea9
yazid113 <92461753+yazid113@users.noreply.github.com>
2022-03-03 20:54:59 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const buttons = document.querySelector('.set');
+buttons.addEventListener('click', handleClick);
+document.addEventListener('keydown', handleKeyDown);
+
+
+function handleClick(e){
+  if (e.target.classList.contains('drum')) {
+      makeSound(e.target.innerText)
+    }
+  }
+function handleKeyDown(e){
+  makeSound(e.key)
+}
+
+function makeSound(key){
+  switch (key) {
+    case 'w':
+        var tom1 = new Audio('sounds/tom-1.mp3');
+        tom1.play()
+      break;
+
+    case 'a':
+        var tom2 = new Audio('sounds/tom-2.mp3');
+        tom2.play()
+      break;
+
+    case 's':
+        var tom3 = new Audio('sounds/tom-3.mp3');
+        tom3.play()
+      break;
+
+    case 'd':
+        var tom4 = new Audio('sounds/tom-4.mp3');
+        tom4.play()
+      break;
+
+    case 'j':
+        var crash = new Audio('sounds/crash.mp3');
+        crash.play()
+      break;
+
+    case 'k':
+        var kickBass = new Audio('sounds/kick-bass.mp3');
+        kickBass.play()
+      break;
+
+    case 'l':
+        var snare = new Audio('sounds/snare.mp3');
+        snare.play()
+      break;
+    default:
+
+  }
+}
